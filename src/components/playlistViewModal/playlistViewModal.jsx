90b954ed56163ea48a7bf75cfab1c4d105b5cd99
Rob00t-unimi <115633385+Rob00t-unimi@users.noreply.github.com>
2023-01-18 21:00:56 +0100

    update

--- src/components/playlistViewModal/playlistViewModal.jsx
+++ src/components/playlistViewModal/playlistViewModal.jsx
@@ -1,12 +1,19 @@
 import {Modal, Button } from 'react-bootstrap'
+import SpotifyWebApi from 'spotify-web-api-node';
 
-//const CLIENT_ID = '61e53419c8a547eabe2729e093b43ae4';
-//const spotifyApi = new SpotifyWebApi({
-//  clientId: CLIENT_ID
-//});
+const CLIENT_ID = '61e53419c8a547eabe2729e093b43ae4';
+const spotifyApi = new SpotifyWebApi({
+  clientId: CLIENT_ID
+});
 
 function PlaylistViewModal({show, onClose, playlist}) {
     
+
+
+    //___________________________________________DA FARE 
+
+
+
     return (
         <Modal show={show}>
             ciao
